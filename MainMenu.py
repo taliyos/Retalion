3d51632c3330f07b6c6fb7bd2a8ae7a5696b10bb
talios0 <charlesreverand.01@gmail.com>
2018-12-19 10:28:54 -0500

    Super cool fade-in text

--- MainMenu.py
+++ MainMenu.py
@@ -7,7 +7,7 @@ class MainMenu():
     """Contains instructions for the main menu"""
     def __init__(this, screen):
         this.screen = screen
-        this.title = Text(screen, "QuickSlot", screen.get_width()/2, 20, (225,240,230), 30, "Fonts/Roboto.ttf", True)
+        this.title = Text(screen, "QuickSlot", screen.get_width()/2, 20, (225,240,230), 30, "Fonts/Roboto.ttf", True, True)
 
     def Update(this, gameState):
         this.title.Draw()
--- Text.py
+++ Text.py
@@ -2,22 +2,41 @@ import pygame
 
 class Text():
     """Text Rendering"""
-    def __init__(this, screen, text, x, y, color, size, font, center):
-        print("hello and welcome to the initializer")
+    def __init__(this, screen, text, x, y, color, size, font, center, fadeInLeft):
         this.screen = screen
         this.text = text
         this.x = x
+        this.desiredX = x
         this.y = y
         this.color = color
+        this.desiredColor = color
         this.size = size
         pygame.font.init() # Initializes the font
         this.font = pygame.font.Font(font, size)
         this.center = center
+        this.fadeInLeft = fadeInLeft
+        if (fadeInLeft):
+            this.x = 0
+            this.color = (0,0,0)
+        this.speed = 1
 
     def Draw(this):
+        if (this.fadeInLeft):
+            this.FadeInLeft()
+
         text = this.font.render(this.text, 1, this.color)
         textInfo = text.get_rect()
         coords = (this.x, this.y)
         if (this.center):
             coords = (this.x - textInfo.center[0], this.y)
         this.screen.blit(text, coords) # Puts text on the screen
+
+    def FadeInLeft(this): # Animation on text
+        if (this.x < this.desiredX):
+            this.x += 1
+        colList = list(this.color)
+        for i in range(len(colList)):
+            if (colList[i] < this.desiredColor[i]):
+                colList[i]+=1
+        this.color = tuple(colList)
+        
