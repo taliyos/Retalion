716138c4746c2a5a36ac61ade2af5cf31963f77b
talios0 <charlesreverand.01@gmail.com>
2018-12-20 08:54:16 -0500

    Standard button visual

--- Button.py
+++ Button.py
@@ -1,4 +1,18 @@
-class Button(object):
-    """description of class"""
+import pygame
+from Text import *
 
+class Button():
+    """Creates a clickable button"""
+    def __init__(this, screen, txt, x, y, sizeX, sizeY, btnCol, txtCol):
+        this.screen = screen
+        this.txt = txt
+        this.x = x
+        this.y = y
+        this.size = (sizeX, sizeY) # A tuple is used here but not for x and y since tuples aren't mutable
+        this.btnCol = btnCol
+        this.txtCol = txtCol
+        this.text = Text(screen, this.txt, this.x, this.y - this.size[1]/2 + 7, this.txtCol, 30, "Fonts/Roboto.ttf",True, False, False, False, 0, 0, 0)
 
+    def Draw(this):
+        pygame.draw.rect(this.screen, this.btnCol, pygame.Rect(this.x-this.size[0]/2, this.y-this.size[1]/2, this.size[0], this.size[1]))
+        this.text.Draw()
--- MainMenu.py
+++ MainMenu.py
@@ -1,5 +1,6 @@
 import pygame
 from Text import *
+from Button import *
 from Display import *
 
 
@@ -8,7 +9,8 @@ class MainMenu():
     def __init__(this, screen):
         this.screen = screen
         this.title = Text(screen, "QuickSlot", screen.get_width()/2, 20, (225,240,230), 30, "Fonts/Roboto.ttf", True, True, True, True, this.screen.get_width()/8, -100, 0.8)
-        this.button = Button(screen, "Play")
+        this.button = Button(screen, "Play", screen.get_width()/2, 300, 200, 50, (100,250,100), (0,0,0))
 
     def Update(this, gameState):
         this.title.Draw()
+        this.button.Draw()
