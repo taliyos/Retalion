d742254d6a28310be5cb2712a6270b13f19ac6fd
talios0 <charlesreverand.01@gmail.com>
2019-01-06 15:11:36 -0500

    Pattern 3

--- BonusEnemy.py
+++ BonusEnemy.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import Vector2
 from random import randint
 from math import *
 
@@ -8,7 +9,6 @@ class BonusEnemy():
         ### Universal Components
         this.screen = screen
         this.events = events
-
         this.player = player
 
         this.center = (this.screen.get_width()/2, this.screen.get_height()/2) # Center of screen
@@ -22,7 +22,7 @@ class BonusEnemy():
         this.level = 0
         this.speed = 10
         this.rotation = []
-        this.stage = -1
+        this.stage = -55
 
         ### Stage 1 Variables
         this.radius = 500
@@ -33,6 +33,10 @@ class BonusEnemy():
         this.fireRate = 2
         this.fireTime = -1
         this.currentLasers = -1
+
+        ### Stage 3 Variables
+        this.stage3Pos = []
+        this.stage3Rot = []
      
     def Update(this):
         if (this.stage == -1):
@@ -42,6 +46,9 @@ class BonusEnemy():
             print("STAGE 2")
             this.LaserPattern2()
             this.stage = 2
+        elif (this.stage < -1):
+            this.LaserPattern3(25, (100,150))
+            this.stage = 5
         this.CheckStage()
         this.Draw()
 
@@ -60,6 +67,7 @@ class BonusEnemy():
         this.rotation.append(rotation * (180/pi) + 90)
 
     def LaserPattern1(this, increment = 4500, storeRotation = True, center = -1):
+        """Circular, Hits a specified portion of the screen"""
         if (center == -1):
             center = this.center
         for rotation in range(-4500,2*31415,increment):
@@ -71,14 +79,41 @@ class BonusEnemy():
             if (storeRotation):
                 rotation = atan2(-y, x)
                 this.rotation.append(rotation * 180/pi + 90)
-
+                
     def LaserPattern2(this):
+        """Circular, Dependent on Player Position"""
         this.currentLasers = -1
         this.fireTime = -1
         this.LaserPattern1(500, False)
-        
+
+    def LaserPattern3(this, lasersPerCorner = 25, center = -1):
+        """X Shape"""
+        # Lasers at each corner
+        if (center == -1):
+            center = this.center
+        placement = Vector2(0,this.screen.get_height())
+        modifier = Vector2(-1,1)
+        for i in range(0,4):
+            print(i)
+            if (i == 1):
+                placement.x = this.screen.get_width()
+                modifier.x = 1
+            elif (i == 2):
+                placement.y = 0
+                modifier.y = -1
+            elif (i == 3):
+                placement.x = 0
+                modifier.x = -1
+            for j in range(0,lasersPerCorner):
+                x = ((j*100) + placement.x) * modifier.x
+                y = ((j*100) + placement.y) * modifier.y
+                this.stage3Pos.append(Vector2(x,y))
+                distance = Vector2(x - center[0], center[1] - y)
+                this.stage3Rot.append(atan2(distance.y, distance.x) * 180/pi + 90)
+
 
     def CheckStage(this):
+        """Adds rotational values to pattern 2 lasers"""
         if (this.stage == 2):
             if (this.currentLasers == len(this.laserX) - 1):
                 return
@@ -114,9 +149,11 @@ class BonusEnemy():
             this.rotation.pop()
             lasersToRemove-=1
 
-        if (len(this.laserX) == 1):
-            this.stage += 1
-
-
-
+        for i in range (0, len(this.stage3Pos) - 1):
+            surface = pygame.transform.rotate(this.laser, this.stage3Rot[i])
+            position = pygame.Rect(this.stage3Pos[i].x-this.size[0]/2, this.stage3Pos[i].y-this.size[1]/2, this.size[0], this.size[1])
+            position = surface.get_rect(center = position.center)
+            this.screen.blit(surface, position)
             
+            this.stage3Pos[i].x -= cos((90-this.stage3Rot[i]) * pi/180) * this.speed
+            this.stage3Pos[i].y -= sin((90-this.stage3Rot[i]) * pi/180) * this.speed
